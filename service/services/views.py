7f406d6f2436d938d1a35d5e928294601ea19fd1
Yana Fedulova <yanafedulovaa@yandex.ru>
2025-11-24 14:05:03 +0300

    оптимизация orm-query + logging

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from clients.models import Client
+from services.models import Subscription
 
-# Create your views here.
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from services.serializers import SubscriptionSerializer
+
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
+    )
+    serializer_class = SubscriptionSerializer 
